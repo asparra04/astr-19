02ba12995f9cb185b18af79b853890b687e7cdcc
asparra04 <adsparra@ucsc.edu>
2024-10-03 10:35:40 -0700

    Session 1 Prompt

---
+++ Session1.py
@@ -0,0 +1,10 @@
+#This program will write my full name and two sentences about my favorite movie and favorite food
+
+def PrintDescription():
+	print("Adrian Parra. My favorite food is brisket, specifically from texas. My favorite movie is the first pacific rim.")
+
+def main():
+	PrintDescription()
+
+if __name__=="__main__":
+	main()
