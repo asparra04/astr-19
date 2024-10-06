632b04e63077d7d1a5b21eb8a1206318f5cdca3f
asparra04 <adsparra@ucsc.edu>
2024-10-06 14:16:55 -0700

    Add files via upload

---
+++ Session2.py
@@ -0,0 +1,13 @@
+#This program will print the sum of two floating numbers
+#The difference between two integers
+#And the product of a floating point number and an integer
+
+def main():
+	x= 5.0
+	y= 3.0 
+	z= 2
+	w= 1
+
+print("x+y = ", x+y)
+print("z-w = ", z-w)
+print("x*z = ", x*z)
